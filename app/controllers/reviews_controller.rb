43eae0a9e54b5fbb12b047af47f7246073fb200a
ytsuji27 <ytsuji91@gmail.com>
2019-03-26 16:43:32 -0700

    Added reviews functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    review = Review.create(review_params)
+    restaurant = Restaurant.find(params[:review][:restaurant_id])
+    if review.valid?
+      redirect_to restaurant_path(restaurant)
+    else
+      flash[:message] = review.errors.full_messages
+      redirect_to restaurant_path(restaurant)
+    end
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :user_id, :restaurant_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :restaurants, through: :favorites
 
   validates :user_name, {presence: true, uniqueness: true}
+  validates :password, presence: true
   
   has_secure_password
 
